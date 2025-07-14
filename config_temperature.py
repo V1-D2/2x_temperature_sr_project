a9adae21249588a5da0600796308dcfc09e58940
Vald Dia <vdidur@wisc.edu>
2025-07-14 15:15:18 -0500

    Reduce number of files

--- config_temperature.py
+++ config_temperature.py
@@ -18,7 +18,7 @@ datasets = {
         },
         'scale_factor': 2,
         'batch_size': 4,
-        'samples_per_file': 3000,  # Ограничение для управления памятью
+        'samples_per_file': 1000,  # Ограничение для управления памятью
         'num_worker': 4,
         'pin_memory': True,
         'persistent_workers': True
